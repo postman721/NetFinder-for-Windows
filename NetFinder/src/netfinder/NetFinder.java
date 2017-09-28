f61a9a470e7c9d15e193e1e5c0e5e7b49abf873f
JJ Posti <jjposti@techtimejourney.net>
2017-09-28 14:01:30 +0300

    program upload

---
+++ NetFinder/src/netfinder/NetFinder.java
@@ -0,0 +1,21 @@
+/*
+NetFinder for Windows v.1 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY; 
+for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under 
+GPL Version 2, June 1991")
+ */
+package netfinder;
+
+/**
+ *
+ * @author JJ Posti - techtimejourney.net
+ */
+public class NetFinder {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GuiFrame gui = new GuiFrame();
+        gui.setVisible(true);
+        System.out.println("NetFinder starts.");}       }
+    
+
